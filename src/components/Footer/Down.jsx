47483809441ee3e3b437d6bb18365e61bf3c05b6
FrancoGarciaa <168785885+FrancoGarciaa@users.noreply.github.com>
2024-11-30 19:47:42 -0300

    Update Down.jsx

--- src/components/Footer/Down.jsx
+++ src/components/Footer/Down.jsx
@@ -7,7 +7,7 @@ return (
         <div className="footer">
             <h2>Copyright 2024 © Mazza|Imports</h2>
     </div>
-    <a href="https://wa.me/5491159758550">
+    <a href="">
     <img src={contactowp} alt="" className="contacto" />
     </a>
     </>
